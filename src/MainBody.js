506ad39063834b51d08f3da56cb88945bc19272f
Oladee <deemajor230600@gmail.com>
2023-03-21 14:53:48 +0100

    almost done

--- src/MainBody.js
+++ src/MainBody.js
@@ -1,10 +1,33 @@
+import Ellipse from "./Ellipse 1.svg";
 export default function MainBody(){
     return (
         <>
         <div className="content">
-            <h5>
-                Reasons i love react
-            </h5>
+            <h4>
+            Fun facts about React
+            </h4>
+            <div>
+               <div>
+                    <img src={Ellipse} alt="bullet" />
+                    <p>Was first released in 2013</p>
+               </div>
+               <div>
+                    <img src={Ellipse} alt="bullet" />
+                    <p>Was originally created by Jordan Walke</p>
+               </div>
+               <div>
+                    <img src={Ellipse} alt="bullet" />
+                    <p>Has well over 100k stars on Github</p>
+               </div>
+               <div>
+                    <img src={Ellipse} alt="bullet" />
+                    <p>Is maintained by facebook</p>
+               </div>
+               <div>
+                    <img src={Ellipse} alt="bullet" />
+                    <p>Powers thousands of entreprise apps, including mobile apps </p>
+               </div>
+            </div>
         </div>
     </>
     )
